98e82532b4bd19934ef1bd91fa600a508a94d745
ralfslima <62228067+ralfslima@users.noreply.github.com>
2026-03-25 09:05:22 -0300

    2ª Aula

---
+++ Aula 02/Conceitos.playground/Contents.swift
@@ -0,0 +1,37 @@
+// Exibir mensagem
+//print("Hello World!")
+
+// Variáveis
+//var nome = "Ralf"
+//print("Bom dia " + nome) // Maneira incorreta
+//print("Bom dia \(nome)") // Maneira correta
+
+// Optional (Quando uma variável pode ser nula)
+//var nome : String? = nil
+//print(nome ?? "Não há nome")
+
+//var gostaDeCafé : Bool = true
+//var 🧑 = true
+//var cachorro = "🐶"
+
+// Constante
+//let nome = "Ralf"
+//let nome : String = "Ralf"
+//nome = "Jean"
+
+/*TIPOS DE DADOS
+ String -> Textual
+ Char   -> Letra/Caractere
+ 
+ Int    -> Números inteiros
+ Float  -> Suporte entre 6 e 7 casas decimais
+ Double -> Double entre 15 e 16 casas decimais
+ UInt   -> Não aceita números negativos
+ 
+ Bool   -> Boolean/Lógica (true / false)
+ 
+ Any       -> Flexível
+ AnyObject -> Classes
+ 
+ Void   -> Quando não há retorno em funções
+*/
